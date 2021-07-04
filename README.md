944d81fa1e2beed9ecc8a539a9fd5a99604f60ca
youk720 <tjm.yk0259@gmail.com>
2021-07-04 13:03:35 +0900

    jul2th_commit

--- README.md
+++ README.md
@@ -2,9 +2,11 @@
 
 M5Stack GRAYで赤外線操作をするリポジトリ
 
+最初は、M5StackのUIflowでやっていたものの、赤外線系はArduinoの方がライブラリ系が充実してるようなので変更しようと思ったものの、Arduinoで使えるようにM5Stackのファームを変更しなければいけないようで、それが思うようにできないという...
+
 参考元:
 ・https://lang-ship.com/blog/work/m5stack-ir-unit/
-・
-・
+・https://qiita.com/nnn112358/items/2f5d33b474e844a09ac3
+・https://qiita.com/taka-murakami/items/a3c05115d5a5038ebb11
 ・
 ・
--- sketch_jun29a/sketch_jun29a.ino
+++ sketch_jun29a/sketch_jun29a.ino
@@ -1,13 +1,11 @@
 //#include <ir_Coolix.h>
 //#include <ir_Sanyo.h>
 //#include <ir_Hitachi.h>
-//#include <ir_Panasonic.h>
 //#include <ir_Vestel.h>
 //#include <ir_Airwell.h>
 //#include <ir_MitsubishiHeavy.h>
 //#include <ir_Midea.h>
 //#include <ir_Gree.h>
-//#include <IRtimer.h>
 //#include <ir_Delonghi.h>
 //#include <ir_Samsung.h>
 //#include <ir_Truma.h>
@@ -15,11 +13,8 @@
 //#include <ir_Kelvinator.h>
 //#include <ir_NEC.h>
 //#include <ir_Electra.h>
-//#include <IRsend.h>
-//#include <IRac.h>
 //#include <ir_Daikin.h>
 //#include <ir_Carrier.h>
-//#include <IRremoteESP8266.h>
 //#include <i18n.h>
 //#include <ir_Trotec.h>
 //#include <ir_Tcl.h>
@@ -27,7 +22,6 @@
 //#include <ir_Corona.h>
 //#include <ir_Neoclima.h>
 //#include <ir_Ecoclim.h>
-//#include <ir_Toshiba.h>
 //#include <ir_Technibel.h>
 //#include <ir_Teco.h>
 //#include <ir_Magiquest.h>
@@ -35,16 +29,24 @@
 //#include <ir_Transcold.h>
 //#include <ir_Haier.h>
 //#include <ir_LG.h>
-//#include <IRutils.h>
 //#include <ir_Whirlpool.h>
 //#include <ir_Voltas.h>
 //#include <ir_Amcor.h>
 //#include <ir_Fujitsu.h>
 //#include <ir_Mitsubishi.h>
-//#include <IRtext.h>
+//赤外線系ライブラリ
+#include <IRtext.h>
 #include <IRrecv.h>
-
-//#include <M5Stack.h>
+#include <IRutils.h>
+#include <IRsend.h>
+#include <IRac.h>
+#include <IRremoteESP8266.h>
+#include <IRtimer.h>
+//メーカー別ライブラリ
+#include <ir_Panasonic.h>
+#include <ir_Toshiba.h>
+//M5Stack用ライブラリ
+#include <M5Stack.h>
 const uint16_t kRecvPin = 36; //受信ピン(ADC対応)
 const uint16_t kCaptureBufferSize = 1024;
 const uint8_t kTimeout = 50;
@@ -65,5 +67,5 @@ void loop() {
   if(irrecv.decode(&results)){
     Serial.println(results.command);
     }
-    delay(1);
+    delay(50);
 }
